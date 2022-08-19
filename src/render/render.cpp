9a7dfd6c91a48e1a25dd1e3b0785bc5e6ecdbbb8
franneck94 <schaffranek.jan@web.de>
2022-08-19 21:59:57 +0200

    up

--- src/render/render.cpp
+++ src/render/render.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <array>
-#include <algorithm>
+#include <cstdint>
 
 #include "imgui.h"
 #include "implot.h"
@@ -9,69 +9,82 @@
 
 void render_window1()
 {
-    ImGui::SetNextWindowPos(ImVec2{ 0.0, 0.0 }, ImGuiCond_FirstUseEver);
-    ImGui::SetNextWindowSize(ImVec2{ 300.0, 150.0 }, ImGuiCond_FirstUseEver);
-    if (ImGui::Begin("window1", nullptr, ImGuiWindowFlags_NoScrollbar))
+    static auto toggle = false;
+
+    ImGui::SetNextWindowPos(ImVec2{ 0.0F, 0.0F });
+    ImGui::SetNextWindowSize(ImVec2{ 110.0F, 120.0F });
+    const auto window_flags = (ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoTitleBar);
+    if (ImGui::Begin("window###1", nullptr, window_flags))
     {
-        if (ImGui::BeginTable("table1", 3, ImGuiTableFlags_Borders))
+        if (ImGui::Button("Click me###1", ImVec2{ 100.0F, 50.0F }))
         {
-            for (int row = 0; row < 4; row++)
-            {
-                ImGui::TableNextRow();
-
-                for (int column = 0; column < 3; column++)
-                {
-                    ImGui::TableSetColumnIndex(column);
-                    ImGui::Text("Cell value");
-                }
-            }
-
-            ImGui::EndTable();
+            toggle = !toggle;
         }
 
-        ImGui::End();
+        if (toggle)
+            ImGui::Text("Active");
+        else
+            ImGui::Text("Inactive");
     }
+    ImGui::End();
 }
 
 void render_window2()
 {
-    ImGui::SetNextWindowPos(ImVec2{ 500.0, 500.0 }, ImGuiCond_FirstUseEver);
-    ImGui::SetNextWindowSize(ImVec2{ 300.0, 150.0 }, ImGuiCond_FirstUseEver);
-    if (ImGui::Begin("window2", nullptr, ImGuiWindowFlags_NoScrollbar))
+    ImGui::SetNextWindowPos(ImVec2{ 400.0F, 400.0F });
+    ImGui::SetNextWindowSize(ImVec2{ 400.0F, 150.0F });
+    const auto window_flags = (ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoTitleBar);
+    if (ImGui::Begin("window###2", nullptr, window_flags))
     {
-        if (ImGui::BeginTable("table2", 3, ImGuiTableFlags_Borders))
+        constexpr auto num_cols = 3;
+        constexpr auto num_rows = 4;
+
+        if (ImGui::BeginTable("table###1", num_cols, ImGuiTableFlags_Borders))
         {
-            for (int row = 0; row < 4; row++)
+            for (int row = 0; row < num_rows; row++)
             {
                 ImGui::TableNextRow();
-
-                for (int column = 0; column < 3; column++)
-                {
-                    ImGui::TableSetColumnIndex(column);
-                    ImGui::Text("Cell value");
-                }
+                ImGui::TableNextColumn();
+                ImGui::Text("Row %d", row);
+                ImGui::TableNextColumn();
+                ImGui::PushStyleColor(ImGuiCol_Text, ImVec4{ 0.0F, 0.0, 1.0F, 1.0F });
+                ImGui::Text("Some contents");
+                ImGui::PopStyleColor();
+                ImGui::TableNextColumn();
+                ImGui::PushStyleColor(ImGuiCol_Text, ImVec4{ 1.0F, 0.0, 0.0F, 1.0F });
+                ImGui::Text("123.456");
+                ImGui::PopStyleColor();
             }
-
             ImGui::EndTable();
         }
-
-        ImGui::End();
     }
+    ImGui::End();
 }
 
 void render_window3()
 {
-    ImGui::SetNextWindowPos(ImVec2{ 900.0, 300.0 }, ImGuiCond_FirstUseEver);
-    ImGui::SetNextWindowSize(ImVec2{ 300.0, 150.0 }, ImGuiCond_FirstUseEver);
-    if (ImGui::Begin("window3", nullptr, ImGuiWindowFlags_NoScrollbar))
-    {
-        if (ImGui::Button("myButton", ImVec2{ 100.0F, 50.0F }))
-        {
-            std::cout << "Clicked\n";
-        }
+    static auto checked = false;
+    static auto slider_value = 0;
 
-        ImGui::End();
+    ImGui::SetNextWindowPos(ImVec2{ 700.0F, 300.0F });
+    ImGui::SetNextWindowSize(ImVec2{ 400.0F, 150.0F });
+    const auto window_flags = (ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoTitleBar);
+    if (ImGui::Begin("window###3", nullptr, window_flags))
+    {
+        ImGui::Text("Text1");
+        ImGui::SameLine();
+        ImGui::Text("Text2");
+
+        ImGui::Separator();
+
+        ImGui::Text("Text3");
+        ImGui::SameLine();
+        ImGui::Text("Text4");
+
+        ImGui::Checkbox("Auto-resize", &checked);
+        ImGui::SliderInt("Slider", &slider_value, 1, 10);
     }
+    ImGui::End();
 }
 
 void render_window4()
@@ -81,37 +94,36 @@ void render_window4()
 
     auto xs1 = std::array<float, size1>{};
     auto ys1 = std::array<float, size1>{};
-    for (std::size_t i = 0U; i < size1; ++i)
+    auto ys2 = std::array<float, size2>{};
+    auto xs2 = std::array<float, size2>{};
+
+    for (int i = 0; i < size1; ++i)
     {
         xs1[i] = i * 0.001f;
-        ys1[i] = 0.5f + 0.5f * sinf(50 * (xs1[i] + static_cast<float>(ImGui::GetTime() / 10)));
+        ys1[i] = 0.5f + 0.5f * sinf(50 * (xs1[i] + (float)ImGui::GetTime() / 10));
     }
-
-    auto xs2 = std::array<float, size2>{};
-    auto ys2 = std::array<float, size2>{};
-    for (std::size_t i = 0U; i < size2; ++i)
+    for (int i = 0; i < size2; ++i)
     {
         xs2[i] = i * 0.1f;
         ys2[i] = xs2[i] * xs2[i];
     }
 
-    ImGui::SetNextWindowPos(ImVec2{ 0.0, 150.0 }, ImGuiCond_FirstUseEver);
-    ImGui::SetNextWindowSize(ImVec2{ 600.0, 350.0 }, ImGuiCond_FirstUseEver);
-    if (ImGui::Begin("window4", nullptr, ImGuiWindowFlags_NoScrollbar))
+    ImGui::SetNextWindowPos(ImVec2{ 200.0F, 200.0F }, ImGuiCond_FirstUseEver);
+    ImGui::SetNextWindowSize(ImVec2{ 400.0F, 150.0F }, ImGuiCond_FirstUseEver);
+    const auto window_flags = (ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoCollapse);
+    if (ImGui::Begin("window###4", nullptr, window_flags))
     {
         ImPlot::CreateContext();
-
         if (ImPlot::BeginPlot("Line Plot"))
         {
             ImPlot::SetupAxes("x", "f(x)");
-            ImPlot::PlotLine("sin(x)", xs1.data(), ys1.data(), static_cast<int>(size1));
+            ImPlot::PlotLine("sin(x)", xs1.data(), ys1.data(), size1);
             ImPlot::SetNextMarkerStyle(ImPlotMarker_Circle);
-            ImPlot::PlotLine("x^2", xs2.data(), ys2.data(), static_cast<int>(size2));
+            ImPlot::PlotLine("x^2", xs2.data(), ys2.data(), size2);
             ImPlot::EndPlot();
         }
-
-        ImGui::End();
     }
+    ImGui::End();
 }
 
 void render()
